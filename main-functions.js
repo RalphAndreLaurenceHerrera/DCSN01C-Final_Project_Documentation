a2e838aa8149b6094de5b5c1088276af1c92f733
Ralph Andre Laurence Herrera <laurenceherrera90@gmail.com>
2024-12-13 17:12:37 +0800

    fourth commit

--- main-functions.js
+++ main-functions.js
@@ -3,37 +3,37 @@ const products = {
         title: "Mais con Yelo",
         description: "A refreshing dessert made with sweet corn, crushed ice, and milk. Perfect for hot weather!",
         image: "https://pilipinasrecipes.com/wp-content/uploads/2018/03/Mais-Con-Yelo-Recipe.jpg",
-        url: "mais-con-yelo.html"
+        url: "https://ralphandrelaurenceherrera.github.io/DCSN01C-Final_Project_Documentation/mais-con-yelo.html"
     },
     ice: {
         title: "Ice Scramble",
         description: "A sweet and slushy treat with powdered milk, chocolate syrup, and colorful sprinkles.",
         image: "https://www.pinoyrecipe.net/wp-content/uploads/2019/06/Ice-Scramble.jpg",
-        url: "ice-scramble.html"
+        url: "https://ralphandrelaurenceherrera.github.io/DCSN01C-Final_Project_Documentation/ice-scramble.html"
     },
     sago: {
         title: "Sago’t Gulaman",
         description: "A classic drink made with tapioca pearls, gelatin, and sweetened arnibal syrup.",
         image: "https://www.kawalingpinoy.com/wp-content/uploads/2018/12/sago-at-gulaman-5.jpg",
-        url: "sagot-gulaman.html"
+        url: "https://ralphandrelaurenceherrera.github.io/DCSN01C-Final_Project_Documentation/agot-gulaman.html"
     },
     mango: {
         title: "Mango Sago",
         description: "A creamy dessert featuring mangoes, sago pearls, and milk, perfect for mango lovers.",
         image: "https://ricelifefoodie.com/wp-content/uploads/2023/04/the-brest-mango-sago-recipe-less-sugar-rice-life-foodie-1-500x500.jpg?crop=1",
-        url: "mango-sago.html"
+        url: "https://ralphandrelaurenceherrera.github.io/DCSN01C-Final_Project_Documentation/mango-sago.html"
     },
     buko: {
         title: "Buko Pandan",
         description: "A delightful dessert with pandan-flavored jelly, young coconut, and sweetened cream.",
         image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS3NpLht_jl2QLffNmFcP1h_v75jTENrmB_lg&s",
-        url: "buko-pandan.html"
+        url: "https://ralphandrelaurenceherrera.github.io/DCSN01C-Final_Project_Documentation/buko-pandan.html"
     },
     halo: {
         title: "Halo-Halo",
         description: "A Filipino dessert made of crushed ice, evaporated milk, and various sweet toppings like beans, jelly, fruits, and flan, mixed together for a refreshing treat.",
         image: "https://www.kawalingpinoy.com/wp-content/uploads/2021/07/halo-halo-7.jpg",
-        url: "halo-halo.html"
+        url: "https://ralphandrelaurenceherrera.github.io/DCSN01C-Final_Project_Documentation/halo-halo.html"
     }
 };
 
